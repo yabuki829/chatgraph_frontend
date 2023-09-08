289a1957a3b42dbf3816638036e1b6d3461b4aa1
yabuki829 <syodai829@icloud.com>
2023-09-08 21:02:00 +0900

    ui 修正

---
+++ components/header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <a href="">info</a>
+    </div>
+  )
+}
+
+export default Header
--- pages/index.jsx
+++ pages/index.jsx
@@ -2,13 +2,10 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 import Head from 'next/head'
 import YouTubeGraph from '../components/youtube_graph';
-
+import Header from '../components/header.jsx';
 export default function Home() {
   const [youtubeUrl, setYoutubeUrl] = useState('');
   const [graphData, setGraphData] = useState(null);
-  const data1 = [1, 2, 3, 4, 5];
-  const data2 = [5, 4, 3, 2, 1];
-  const labels = ['A', 'B', 'C', 'D', 'E'];
 
   const instance = axios.create({
 
@@ -18,7 +15,9 @@ export default function Home() {
       "Accept": "application/json",
       "Content-Type": "application/json",      
     },
+
   })
+
   const handleSubmit = async (e) => {
     e.preventDefault();
 
@@ -39,30 +38,36 @@ export default function Home() {
   return (
     
     <div>
-   
        <Head>
         <title>草カウンターfor youtube</title>
       </Head>
-    <h1>YouTubeチャット草カウンター</h1>
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="youtube_url">YouTube動画URL:</label>
-      <input
-        type="text"
-        id="youtube_url"
-        value={youtubeUrl}
-        onChange={(e) => setYoutubeUrl(e.target.value)}
-      />
-      <button type="submit">解析</button>
-    </form>
+      <Header/>
+      <div className='mt-32 h-16 flex flex-col justify-center items-center  '>
+        <h1 className='font-bold text-2xl md:text-3xl'>草カウンター for Youtube</h1>
+       
+      </div>
+      
+      <form className='w-full flex flex-col justify-center items-center mb-20' onSubmit={handleSubmit}>
+          <input
+            className='rounded-full bg-gray-100 w-3/4 md:w-2/5 h-12 px-4 md:m-auto '
+            type="text"
+            id="youtube_url"
+            value={youtubeUrl}
+            placeholder="Youtubeのurlを入力"
+            onChange={(e) => setYoutubeUrl(e.target.value)}
+          />
+        </form>
+     
 
     {graphData && (
  
-     
       <YouTubeGraph videoId={graphData.data.youtube_id} 
                     data1={graphData.data.comment_counts} 
                     data2={graphData.data.grass_counts} 
                     labels={graphData.data.intervals}/>
     )}
+   <br />
+   <br />
   </div>
   )
 }
